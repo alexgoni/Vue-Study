625796aa6f7401e0b81db90a2a7f550df6c9f68f
alexgoni <goni000211@gmail.com>
2024-11-28 20:43:26 +0900

    feat: 숫자 야구

--- baseball-number/NumberBaseball.vue
+++ baseball-number/NumberBaseball.vue
@@ -1,32 +1,82 @@
 <template>
   <div>
-    <h1>{{ result }}</h1>
     <form @submit.prevent="onSubmitForm">
-      <input ref="answer" type="number" maxlength="4" v-model="value" />
+      <input ref="answer" minlength="4" maxlength="4" v-model="value" />
+      <button type="submit">입력</button>
     </form>
-    <div>시도: {{ result }}</div>
+    <div>시도: {{ tries.length }}</div>
+    <ul>
+      <li v-for="(t, index) in tries" :key="index">
+        <div>{{ t.try }}</div>
+        <div>{{ t.result }}</div>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
+const getNumbers = () => {
+  const candidates = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const array = [];
+
+  for (let i = 0; i < 4; i++) {
+    const chosenIdx = Math.ceil(Math.random() * 9) - 1;
+    array.push(candidates[chosenIdx]);
+  }
+
+  return array;
+};
 
 export default {
-  setup() {
-    const value = ref("");
-    const result = ref("");
-
-    const onSubmitForm = (e) => {
-      // e.preventDefault()는 @submit.prevent로 처리됩니다.
-    };
-
+  data() {
     return {
-      value,
-      result,
-      onSubmitForm,
+      answer: getNumbers(),
+      tries: [],
+      value: "",
+      result: "",
     };
+  },
+  methods: {
+    onSubmitForm() {
+      let strike = 0;
+      let ball = 0;
+      const valueArr = [...this.value].map((each) => Number(each));
+
+      const compareValueAndAnswer = () => {
+        for (let i = 0; i < 4; i++) {
+          if (this.answer[i] === valueArr[i]) strike++;
+          else if (this.answer.includes(valueArr[i])) ball++;
+        }
+
+        if (strike === 4) this.result = "홈런";
+        else this.result = `${strike}스트라이크 ${ball}볼`;
+        this.tries.push({
+          try: this.value,
+          result: this.result,
+        });
+      };
+
+      const resetGame = () => {
+        const confirm = window.confirm("게임을 다시 시작할까요?");
+        if (confirm) {
+          this.tries = [];
+          this.answer = getNumbers();
+          this.result = "";
+        }
+      };
+
+      compareValueAndAnswer();
+      if (this.result === "홈런") resetGame();
+      else if (this.tries.length >= 10) {
+        alert(`정답은 ${this.answer.join("")} 입니다!`);
+        resetGame();
+      }
+
+      this.value = "";
+      this.$refs.answer.focus();
+    },
   },
 };
 </script>
 
-<style scoped></style>
+<style></style>
